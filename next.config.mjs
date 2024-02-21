dc7fd95cb9ceaae0bd22826f86de9380514af4dd
Calvin Ellis <cellis9421@gmail.com>
2024-02-21 11:54:10 -0500

    fix: Add workaround for static output config on dev server

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = { };
+
+
+// Check environment we're running in
+const isDev = process.env.NODE_ENV === 'development';
+
+// Setting output and distDir based on environment to avoid a known bug with Next.js dev server and this configuration
+// @see https://stackoverflow.com/questions/77578942/missing-required-error-components-refreshing-next-js
+if (!isDev) {
+    // set output to 'export' to build static files
+    nextConfig.output = 'export';
+    // set output directory to 'docs' for github pages
+    nextConfig.distDir = 'docs';
+}
 
 export default nextConfig;
