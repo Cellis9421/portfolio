036b29a01f183d4b5d4360bc552e7c9194fc5257
Calvin Ellis <cellis9421@gmail.com>
2026-02-04 14:06:30 -0500

    chore: Add Prettier configuration and ignore files

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  printWidth: 180,
+  singleQuote: true,
+  trailingComma: 'es5',
+  semi: true,
+  bracketSpacing: true,
+  endOfLine: 'auto',
+};
